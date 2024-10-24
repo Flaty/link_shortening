d6813e1bca5fc7f7d4e915ee632a45fb3ece4699
Flaty <78325079+Flaty@users.noreply.github.com>
2024-10-24 22:52:03 +0300

    Исправил названия, логику некоторых функций и высушил код

--- vk.py
+++ vk.py
@@ -1,18 +1,19 @@
 import requests
-import sys
+from urllib.parse import urlparse
 from environs import Env
 env = Env()
 env.read_env()
 
 
-ACCESS_TOKEN = env.str('ACCESS_TOKEN')
+VK_TOKEN = env.str('VK_TOKEN')
 
 
 def is_shorten_link(url):
-    if url[:14] in 'https://vk.cc/':
-        print('Статистика по кликам: ', count_clicks(ACCESS_TOKEN, url))
+    parsed_url = urlparse(url)
+    if parsed_url.netloc in 'vk.cc':
+        return True
     else:
-        print('Сокращенная ссылка: ', shorten_link(ACCESS_TOKEN, url))
+        return False
 
 
 def shorten_link(token, url):
@@ -26,16 +27,18 @@ def shorten_link(token, url):
     }
 
     response = requests.get('https://api.vk.com/method/utils.getShortLink', headers=headers, params=params)
-
-    data = response.json()
-    if 'response' in data:
-        return data['response']['short_url']
+    response.raise_for_status()
+    response_data = response.json()
+    if 'response' in response_data:
+        short_link = response_data['response']['short_url']
+        return print(f'Ваша сокращенная ссылка: {short_link}')
     else:
         raise requests.exceptions.HTTPError
 
 
 def count_clicks(token, link):
-    link = link[14:]
+    parsed_url = urlparse(link)
+    link = parsed_url.path.split('/')
     headers = {
         'Authorization': f'Bearer {token}'
     }
@@ -46,9 +49,13 @@ def count_clicks(token, link):
         
     }
     response = requests.get('https://api.vk.com/method/utils.getLinkStats', headers=headers, params=params)
-    data = response.json()
-    if 'response' in data:
-        return data['response']['stats'][0]['views']
+    response.raise_for_status()
+    response_data = response.json()
+    if 'response' in response_data:
+        if response_data['response'].get('stats', []):
+            stats_link = response_data['response']['stats'][0]['views']
+            return print(f'Количество переходов по вашей ссылке: {stats_link}')
+        print('Статы пока нету :(')
     else:
         raise requests.exceptions.HTTPError
 
@@ -56,7 +63,9 @@ def count_clicks(token, link):
 if __name__ == "__main__":
     user_input = input('Введите свою ссылку для сокращения: ')
     try:
-        is_shorten_link(user_input)
+        if is_shorten_link(user_input):
+            count_clicks(VK_TOKEN, user_input)
+        else:
+            shorten_link(VK_TOKEN, user_input)
     except requests.exceptions.HTTPError:
         print('Вы ввели неправильную ссылку')
-        sys.exit()
