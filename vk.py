a59e66cc7d90f65fbac98577cf97374c4da5469a
Flaty <78325079+Flaty@users.noreply.github.com>
2024-10-26 13:04:37 +0300

    Удалил .env и убрал потенциальный баг

--- vk.py
+++ vk.py
@@ -1,19 +1,23 @@
 import requests
 from urllib.parse import urlparse
 from environs import Env
-env = Env()
-env.read_env()
 
 
-VK_TOKEN = env.str('VK_TOKEN')
+def is_shorten_link(token, url):
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
 
+    params = {
+        'v': '5.199',
+        'url': url
+    }
 
-def is_shorten_link(url):
+    response = requests.get('https://api.vk.com/method/utils.checkLink', headers=headers, params=params)
+    response.raise_for_status()
+    response_data = response.json()
     parsed_url = urlparse(url)
-    if parsed_url.netloc in 'vk.cc':
-        return True
-    else:
-        return False
+    return parsed_url.netloc in 'vk.cc' and 'not_banned' in response_data['response']['status']
 
 
 def shorten_link(token, url):
@@ -31,7 +35,7 @@ def shorten_link(token, url):
     response_data = response.json()
     if 'response' in response_data:
         short_link = response_data['response']['short_url']
-        return print(f'Ваша сокращенная ссылка: {short_link}')
+        return print(short_link)
     else:
         raise requests.exceptions.HTTPError
 
@@ -46,26 +50,28 @@ def count_clicks(token, link):
     params = {
         'v': '5.199',
         'key': link
-        
     }
     response = requests.get('https://api.vk.com/method/utils.getLinkStats', headers=headers, params=params)
     response.raise_for_status()
     response_data = response.json()
     if 'response' in response_data:
-        if response_data['response'].get('stats', []):
-            stats_link = response_data['response']['stats'][0]['views']
-            return print(f'Количество переходов по вашей ссылке: {stats_link}')
-        print('Статы пока нету :(')
+        stats_link = response_data['response']['stats'][0]['views']
+        return print(stats_link)
     else:
         raise requests.exceptions.HTTPError
 
 
 if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+
+    vk_token = env.str('VK_TOKEN')
+
     user_input = input('Введите свою ссылку для сокращения: ')
     try:
-        if is_shorten_link(user_input):
-            count_clicks(VK_TOKEN, user_input)
+        if is_shorten_link(vk_token, user_input):
+            count_clicks(vk_token, user_input)
         else:
-            shorten_link(VK_TOKEN, user_input)
+            shorten_link(vk_token, user_input)
     except requests.exceptions.HTTPError:
         print('Вы ввели неправильную ссылку')
